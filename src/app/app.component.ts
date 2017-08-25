319064648d653dd3b02b751ae006d149ab8a330b
Wink Saville <wink@saville.com>
2017-08-25 11:23:25 -0700

    Add synchronous service
    Creates and Injectable HeroService, hero.service.ts plus mock-heroes.ts, with a getHeroes method to return the
    list of heroes.
    
    Also add a private heroService property to AppComponent which is the
    target for a HeroService provider.
    
    Changed AppComponent to implement OnInit interface, add ngOnInit, to
    initialize the private heroService property.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,6 @@
-import { Component } from '@angular/core';
-
-export class Hero {
-  id: number;
-  name: string;
-}
-const HEROES: Hero[] = [
-      {id: 11, name: 'Mr. Nice'},
-      {id: 12, name: 'Narco'},
-      {id: 13, name: 'Bambasto'},
-      {id: 14, name: 'Celeritas'},
-      {id: 15, name: 'Magenta'},
-      {id: 16, name: 'RubberMan'},
-      {id: 17, name: 'Dynama'},
-      {id: 18, name: 'Dr IQ'},
-      {id: 19, name: 'Magma'},
-      {id: 20, name: 'Tornado'},
-];
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
@@ -80,11 +65,22 @@ const HEROES: Hero[] = [
       border-radius: 4px 0 0 4px;
     }
   `],
+  providers: [HeroService],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService) {}
+
+  updateHeros(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  ngOnInit(): void {
+    this.updateHeros();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
