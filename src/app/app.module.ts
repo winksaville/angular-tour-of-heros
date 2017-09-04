576af2823ca66b00e8e179a9674e93921c38f8eb
Wink Saville <wink@saville.com>
2017-09-04 16:23:49 -0700

    Add in-memory-data.service
    Instead of mock-heroes.ts we now have a in-memory-data.service which
    simulates a web service.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard.component';
@@ -11,16 +16,18 @@ import { HeroService } from './hero.service';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
     HeroesComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
